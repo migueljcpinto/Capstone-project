3a7f21ec0cb569a0ad168dd4b9c0a37909e42c3b
Miguel Pinto <migueljcpinto@gmail.com>
2023-08-07 12:17:45 +0200

    API endpoint corrections

--- pages/api/nurses/[id].js
+++ pages/api/nurses/[id].js
@@ -34,6 +34,17 @@ export default async function handler(request, response) {
       response.status(500).json({ status: "Error updating nurse." });
     }
   }
+
+  if (request.method === "POST") {
+    try {
+      const nurseData = request.body;
+      const newNurse = await Nurse.create(nurseData);
+      return response.status(201).json(newNurse);
+    } catch (error) {
+      return response.status(500).json({ error: "Error adding nurse." });
+    }
+  }
+
   if (request.method === "DELETE") {
     try {
       await Nurse.findByIdAndDelete(id);
