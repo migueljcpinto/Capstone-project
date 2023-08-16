138384e09041daf6695cf49e7384106478fea08c
Miguel Pinto <migueljcpinto@gmail.com>
2023-08-16 09:40:51 +0100

    models nurseworkdates

--- db/models/Nurse.js
+++ db/models/Nurse.js
@@ -1,4 +1,5 @@
 import mongoose from "mongoose";
+import "./NurseWorkDates";
 
 const { Schema } = mongoose;
 
@@ -10,6 +11,11 @@ const nurseSchema = new Schema({
   hoursPerWeek: { type: Number, required: true },
   specialist: { type: Boolean, required: true },
   image: { type: String },
+  email: { type: String, unique: true, required: true },
+  phoneNumber: { type: Number, required: true },
+  description: { type: String, required: true },
+  workSchedule: { type: [Schema.Types.ObjectId], ref: "NurseWorkDates" },
+  //define that it is an array of Object-Ids and has a reference to the `NurseWorkDates` schema
 });
 
 const Nurse =
